1eaa49b333068161b35e11dd44e09afdc8c3f9f3
sagor1155 <sagor.eee.cuet@gmail.com>
2020-05-26 03:20:46 +0600

    minor comment added

--- thread-condition-variable.cpp
+++ thread-condition-variable.cpp
@@ -7,9 +7,15 @@ NOTES:
    a. notify_one();
    b. notify_all();
 2. You need mutex to use condition variable
-3. Condition variable is used to synchronize two or more threads.
-4. Best use case of condition variable is Producer/Consumer problem.
-5. Condition variables can be used for two purposes:
+3. If some thread want to wait on some condition variable then it has to do these things:
+   a. Acquire the mutex lock using std::unique_lock<std::mutex> lock(m);
+   b. Execute wait/wait_for/wait_untill. The wait operations automatically release the mutex and suspend
+      the execution of the thread. 
+   c. When the condition variable is notified, the thread is awakened, and the mutex is automatically reacquired.
+      The thread should then check the condition and resume waiting if the wakeup was spurious. 
+4. Condition variable is used to synchronize two or more threads.
+5. Best use case of condition variable is Producer/Consumer problem.
+6. Condition variables can be used for two purposes:
     a. Notify other threads
     b. Wait for some condition
 */
@@ -50,6 +56,7 @@ void withdrawMoney(int money){
     /*
     if condition is false: cv.wait will release mutex and wait here. 
     if condition is  true: cv.wait will lock mutex and proceed to next statement   
+    if someone notifies then it acquires the lock and checks the condition inside 
     */
     if(balance >= money){
         balance -= money;
